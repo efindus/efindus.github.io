3380a7a9669886fd19c182e57bdefac65cc7d3be
efindus <48939809+efindus@users.noreply.github.com>
2021-03-10 21:50:26 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-
+let score = 0
+document.getElementById('baton').addEventListener('click', () => {
+  score++
+  document.getElementById('daSkor').innerHTML = score
+  if (score === 2137) {
+    document.getElementById('blogoslawieni').play()
+  }
+}, false);
