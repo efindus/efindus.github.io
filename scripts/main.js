2199db010c60cf97b557b7d57697937a78ef2c4b
efindus <48939809+efindus@users.noreply.github.com>
2021-03-10 22:21:31 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,29 @@
 let score = 0
+
+let handleAudio = () => {
+  let audio = document.getElementById('blogoslawieni')
+    
+  if (!audio.paused)
+    if (audio.currentTime > 37)
+      audio.currentTime = 0
+  else {
+    audio.currentTime = 0
+    audio.play()
+  }
+}
+
 document.getElementById('baton').addEventListener('click', () => {
   score++
   document.getElementById('daSkor').innerHTML = score
   if (score === 2137) {
-    document.getElementById('blogoslawieni').play()
+    handleAudio()
   }
 }, false);
+
+setInterval(() => {
+  let date = new Date()
+  
+  if (date.getHours() === 21 && date.getMinutes() === 37) {
+    handleAudio()
+  }
+}, 10000)
